cbaa9e558e24a29de7f0b0ebff100baa4ab969cf
Ricep Ardiansyah <ardiansyahricep@gmail.com>
2024-10-14 23:11:16 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+
+
+
+app.use('/products', productRoutes);
+
+module.exports = app;
+
